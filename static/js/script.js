5bb7c9feb216f8755355b6e4eb6d527722ad985c
tanaykulkarni27 <1002kulkarnitp@gmail.com>
2022-03-22 09:41:31 +0530

    project bugs fixed

--- static/js/script.js
+++ static/js/script.js
@@ -182,3 +182,37 @@ var swiper = new Swiper(".blogs-slider", {
   },
 });
 
+function sendLogin(){
+  var u_name = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+  if(u_name.length <= 0){
+    alert("username cannot be empty");
+    return;
+  }
+  if(password.length <= 0){
+    alert("password cannot be empty");
+    return;
+  }
+  $.ajax({
+    type : "POST",
+    url  : "http://localhost:8000/login",
+    data : {
+      "username" : u_name,
+      "password" : password,
+    },
+    beforeSend:function(){
+      // alert("GOing to send request");
+      $("#buffer").show();
+    },
+    success : function(e){
+      $("#buffer").hide();
+      if(e == "done"){
+        window.location.replace("http://localhost:8000/");
+        
+      }
+      else{
+        alert(e);
+      }
+    }
+  });
+}
